b327c31c58526e7ea2bf95b63ec30a5e87fc43d6
tuzilingdang <853651371@qq.com>
2019-02-22 11:31:50 +0800

    fix(app.vue): Adapt to iPhoneX

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,15 @@
 </script>
 
 <style lang='less'>
+    body {
+        // padding-top: constant(safe-area-inset-top);
+        // padding-left: constant(safe-area-inset-left); //如果未竖屏时为0                
+        // padding-right: constant(safe-area-inset-right); //如果未竖屏时为0                
+        // padding-bottom: constant(safe-area-inset-bottom); //为底下圆弧的高度 34px    
+        padding-bottom: constant(safe-area-inset-bottom);
+        padding-bottom: env(safe-area-inset-bottom);
+    }
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -31,14 +40,21 @@
         margin-top: 2vh;
 
         #game {
-            width: 90%;
+            width: 85%;
             max-width: 360px;
-            height: 88vh;
-            border: #c5bab9 3px solid;;
+            max-height: 610px;
+            height: 94vh;
+
+            @media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+                // iphoneX css部分
+                height: 86vh;
+                width: 90%;
+            }
+
+            border: #c5bab9 3px solid;
             border-radius: 40px;
             margin: auto;
-            padding-top: 15%;
-            // background: #3c3a3a;
+            padding-top: 10vh;
             background: -webkit-gradient(linear, 0% 0%, 45% 0%, from(#3C3A3A), to(#3C3A3A), color-stop(0.0, #6B6868));
             box-shadow: 10px 8px 5px #999;
         }
